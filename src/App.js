fca7137baae70257a72ec6ee8214b2a358e91a51
Lukasz <lukaszprzybylka@gmail.com>
2022-11-13 11:09:03 +0000

    added filter functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import { useState } from "react";
 
 const App = () => {
-
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-  ])
+    { name: "Arto Hellas", phone: "040-123456", id: 1 },
+    { name: "Ada Lovelace", phone: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", phone: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", phone: "39-23-6423122", id: 4 },
+    { name: "Movda Hellas", phone: "39-23-12341234", id: 5 }
+  ]);
 
   const [newPerson, setNewPerson] = useState({
     name: "",
     phone: "",
   });
 
-const [serachedPerson, setSerachedPerson] = useState({
-  name: "",
-  phone: ""
-})
-  
+  const [serachedPerson, setSerachedPerson] = useState("");
 
   // on the form submission we prevent the default behaviour of the event and then we create a new object with the name from the input
   // next we will add that name to the state Persons
@@ -28,7 +24,7 @@ const [serachedPerson, setSerachedPerson] = useState({
       name: newPerson.name,
       phone: newPerson.phone,
     };
-    if (persons.find((person) => person.name === newPerson.name)) {
+    if (persons.find((person) => person.name === serachedPerson.name)) {
       alert(`${newPerson.name} is already added to phonebook`);
     } else {
       setPersons(persons.concat(nameObject));
@@ -39,22 +35,41 @@ const [serachedPerson, setSerachedPerson] = useState({
     }
   };
 
+  const List = ({ array }) => {
+    if (serachedPerson) {
+      return array
+        .filter((person) => person.name.toLowerCase().includes(serachedPerson.toLowerCase()))
+        .map((person) => (
+          <li key={person.id}>
+            <strong>Name:</strong> {person.name} <strong>Phone:</strong>{" "}
+            {person.phone}
+          </li>
+        ));
+    } else {
+      return array.map((person) => (
+        <li key={person.id}>
+          <strong>Name:</strong> {person.name} <strong>Phone:</strong>{" "}
+          {person.phone}
+        </li>
+      ));
+    }
+  };
 
   // https://www.pluralsight.com/guides/handling-multiple-inputs-with-single-onchange-handler-react refactor changeHandler functions to 1
   const handleNameChange = (event) => {
-    console.log(event.target.value);
     setNewPerson({ ...newPerson, name: event.target.value });
   };
   const handlePhoneChange = (event) => {
-    console.log(event.target.value);
     setNewPerson({ ...newPerson, phone: event.target.value });
   };
+  const handleSearchChange = (event) => setSerachedPerson(event.target.value);
 
   return (
     <div>
       <h2>Phonebook</h2>
       <div>
-        <input value={serachedPerson.name}/>
+        Serach:{" "}
+        <input value={serachedPerson.name} onChange={handleSearchChange} />
       </div>
       <h2>add a new:</h2>
       <form onSubmit={addName}>
@@ -69,13 +84,8 @@ const [serachedPerson, setSerachedPerson] = useState({
         </div>
       </form>
       <h2>Numbers</h2>
-        <ul>
-        {persons.map((person) => (
-          <li key={person.id}>
-            <strong>Name:</strong> {person.name} <strong>Phone:</strong>{" "}
-            {person.phone}
-          </li>
-        ))}
+      <ul>
+        <List array={persons} />
       </ul>
     </div>
   );
